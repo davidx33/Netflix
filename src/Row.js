846370ee10b103ff58bfa0490128d3090fd7d8e4
David Xu <david.xu@duke.edu>
2022-06-04 15:14:55 -0400

    some changes

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import requests from './requests';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
 // snippet code runs based on condition
@@ -10,9 +11,12 @@ function Row({ title }) {
 // if array blank, only run once when row loads
         async function fetchData() {
             const request = await axios.get(fetchUrl);
+            // console.log(request.data.results)
+            setMovies(request.data.results);
+            return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
 
     return (
     <div>
