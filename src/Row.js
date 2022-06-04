9151193ca53db36b4a271bfc11c8eb4374cc1b21
David Xu <david.xu@duke.edu>
2022-06-04 16:21:19 -0400

    put in some css

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from './requests';
+import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original"
 
@@ -23,9 +24,9 @@ function Row({ title, fetchUrl }) {
     return (
     <div className='row'>
         <h2>{title}</h2>
-        <div className='row_posters'>
+        <div className='row__posters'>
             {movies.map(movie => (
-                <img className='row_poster'
+                <img className='row__poster'
                 src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
             ))}
         </div>
