c70533233c3072c421bd93ef47973ff3a9e66a77
David Xu <david.xu@duke.edu>
2022-06-04 23:06:14 -0400

    finished the banner

--- src/Nav.js
+++ src/Nav.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from 'react'
 import "./Nav.css"
 
 function Nav() {
-    const [show, handleShow] = useState(false)
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            } else handleShow(false);
-        });
+    // const [show, handleshow] = useState(false)
 
-        return () => {
-            window.removeEventListener("scroll")
-        }
+    // useEffect(() => {
+    //   window.addEventListener("scroll", () => {
+    //     if (window.scrollY > 100) {
+    //       handleshow(true)
+    //     } else handleshow(false);
+    //   });
+    //   return () => {
+    //     window.removeEventListener("scroll")
+    //   }
+    // }, []);
 
-    }, []);
+
   return (
-    <div className={`nav ${show && "nav__black"}`}>
+    <div className = "nav" >
         <img className='nav__logo'
         src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png" 
         alt = "Netflix Logo"
