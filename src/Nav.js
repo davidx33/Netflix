6aab871aa21942690d39dade95f9c33d271b6466
David Xu <david.xu@duke.edu>
2022-06-04 22:36:31 -0400

    stuck on bug

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Nav.css"
 
 function Nav() {
+    const [show, handleShow] = useState(false)
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+
+        return () => {
+            window.removeEventListener("scroll")
+        }
+
+    }, []);
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav__black"}`}>
         <img className='nav__logo'
         src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png" 
         alt = "Netflix Logo"
