1fc6c090e3b644365e980c547ee80d51457850af
David Xu <david.xu@duke.edu>
2022-06-04 21:33:06 -0400

    making the styles look right

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css"
 
 function Banner() {
 
@@ -20,8 +21,32 @@ function Banner() {
     }, []);
 
     return (
-        <header>
-        
+        <header className='banner'
+        style={{
+            backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center"
+        }}>
+            <div className='banner__contents'>
+                <h1 className='banner__title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+            <div className='banner__buttons'>
+                <button className='banner__button'>Play</button>
+                <button className='banner__button'>My List</button>
+            </div>
+
+            <h1 className='banner__description'>
+            {movie?.overview}
+            </h1>
+
+            {/* div > 2 buttons */}
+            {/* description of the show/movie */}
+
+            </div>
+               
         </header>
   )
 }
